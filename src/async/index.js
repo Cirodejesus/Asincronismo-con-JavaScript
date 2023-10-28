7aadd267862c7c53fddb4ca701b3ad938a7aa5f5
Cirodejesus <cjarrieta2507@hotmail.com>
2023-10-27 19:10:46 -0500

    work asinc-await

--- src/async/index.js
+++ src/async/index.js
@@ -11,12 +11,13 @@ const anotherFn = async () => { //la palabra async es para el cuerpo de la funci
     const something = await fnAsync(); //aquí nos está regresando una promesa, o que hace que el programa espere hasta que la variable(promesa) se resuelva para continuar..
     console.log(something); //se imprime mientras se espera
     console.log('Hello!');
+    console.log('Gorra');
 }
 
 console.log('Before'); //al ser la primera orden con solo console.log, 'Before' se imprime primero
 anotherFn();//es el segundo en llamar, pero aún así no se imprimen los console de su lógica y tarda 2 s en ser ejecutada
 console.log('After'); //aparece justo después de 'Before' porque anotherFn() está esperando una promesa y aún así el programa no se detiene, sino que sigue y así tenemos 'After' de segundo al imprimir
-
+console.log('Antonio');
 /*La salida al correr con Run Code queda:
 Before
 After
